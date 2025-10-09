fd576486525f326ff9411209e136830e4c407d6f
lohikrme <chinese.parrotlet@gmail.com>
2025-10-09 11:59:25 +0300

    udp echo server and client work together, documentation for them

---
+++ Distributed-Software/udp-echo-client/udp-echo-client.cpp
@@ -0,0 +1,167 @@
+// udp-echo-client.cpp
+// g++ -std=c++14 udp-echo-client.cpp -o udp-echo-client
+
+// u could also conditionally compile with -D tag 
+// e.g g++ -std=c++14 -D LINUX udp-echo-client.cpp -o udp-echo-client
+// but this software is not made to support it
+// the condition we do is #if 0 or else
+
+// FOr UDP echo client the parameters are
+// -h = host ip address, -p = port, -m = message
+// ./udp-echo-client -h ip -p 12700 -m "message"
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+
+#define DEFAULT_SERVER_PORT 8080
+#define MAXSIZE 1000+32
+
+// decide do u make software for linux or windows
+// u can also give them in compiling with tag -D e.g -D LINUX
+#define LINUX
+// #define WINDOWS
+
+// typedef allows defining own type
+typedef struct _config {
+    char *host = NULL;
+    char *message = NULL;
+    int port;
+    int timeout = 10;
+} Config_t;
+
+// function prototypes mean order of writing functions doesn't matter
+void GetCmdLineOptions( int argc, char *argv[], Config_t *pconfig);
+int main(int argc, char *argv[]);
+//------------------------------------------------------------------------
+
+// GetCmdLineOptions() is a function, that reads given extra tags after calling the main software
+// e.g ./udp-echo-server -p nnn would be summon of main software, and nnn would be data associated with -p tag
+
+void GetCmdLineOptions( int argc, char *argv[], Config_t* pconfig) {
+    int opt;
+    // getopt searches for options with tags, such as "-p" and takes its value such as "nnn"
+    // -t tag = timeout
+    while((opt=getopt(argc, argv, "h:p:m:t:")) != -1) {
+        switch(opt) {
+            case 'h':
+                pconfig->host = optarg;
+                break;
+            case 'p':
+                // mene osoittimen pconfig osoittamaan rakenteeseen ja käytä sen port-kenttää
+                pconfig->port = atoi(optarg);
+                break;
+            case 'm':
+                pconfig-> message = optarg;
+                break;
+            case 't':
+                pconfig -> timeout = atoi(optarg);
+                break;
+            case '?':
+            fprintf(stderr, "Unknown option %c\n", optopt);
+                exit(EXIT_FAILURE);
+                break;
+            default:
+                fprintf(stderr, "Usage: udp-echo-client -h host -p port -m message\n");
+        }
+    }
+}
+
+
+int main(int argc, char *argv[]) {
+    // make a variable called config that is type of Config_t
+    Config_t config;
+    // save socket id into a variable, when socket is created, OS gives it a number
+    int sockid, len, n;
+    sockid = len = n = 0;
+    struct sockaddr_in serveraddr;
+    char in_buf[MAXSIZE];
+
+    // read at the start of invoking main function the parameters main is created with
+    GetCmdLineOptions(argc, argv, &config);
+
+    if(config.message == NULL) {
+        perror("No message to send");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((sockid=socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        // if problem, print error
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&serveraddr, 0, sizeof(serveraddr));
+
+// conditional compiling
+#if 0
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_port = htons(config.port);
+    serveraddr.sin_addr.s_addr = inet_addr(config.host);    
+#else 
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_port = htons(config.port);
+
+    struct hostent *h;
+    struct in_addr* *addrList;
+    char host_ip[60];
+
+    if( (h=gethostbyname(config.host)) == NULL) {
+        printf("gethostbyname error... using original\n");
+        // copy from config ip to host_ip
+        strcpy(host_ip, config.host);
+    } 
+    else {
+        addrList = (struct in_addr**) h->h_addr_list;
+        // inet_ntoa() converts IP address from binary to human form
+        strcpy(host_ip, inet_ntoa(*addrList[0]));
+    }
+    if (inet_pton(AF_INET, host_ip, &serveraddr.sin_addr.s_addr) <= 0) {
+        perror("Invalid IP-address");
+        exit(EXIT_FAILURE);
+    }
+    printf("host %s -> host-ip %s\n", config.host, host_ip);
+#endif
+
+    struct timeval tv;
+    tv.tv_sec = config.timeout;
+    tv.tv_usec = 0;
+
+    if (setsockopt(sockid, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0) {
+        perror("Error in setting the timeout for recvfrom!");
+    }
+
+    // just send an array of characters (string) to server, server handles \0 etc
+    sendto(
+        sockid, 
+        config.message, 
+        strlen(config.message), 
+        MSG_CONFIRM, (const sockaddr*) &serveraddr, 
+        sizeof(serveraddr)
+    );
+    printf("Messageto %s:%d sent %s", config.host, config.port, config.message);
+
+    if((n = recvfrom(sockid, in_buf, MAXSIZE-1, MSG_WAITALL, (sockaddr*) &serveraddr, (socklen_t *) &len)) < 0) {
+        perror("recvfrom failed");
+        exit(EXIT_FAILURE);
+    }
+    if (n == 0) {
+        perror("Socket closed by server");
+        exit(EXIT_FAILURE);
+    }
+    in_buf[n] = '\0';
+    printf("Server returned: %s\n", in_buf);
+    close(sockid);
+    return 0;
+}
--- Distributed-Software/udp-echo-server/udp-echo-server.cpp
+++ Distributed-Software/udp-echo-server/udp-echo-server.cpp
@@ -2,6 +2,9 @@
 $ g++ -std=c++14 udp-echo-server.cpp -o udp-echo-server
 */
 
+// start server in linux with command (p = port, d = delay)
+// ./udp-echo-server -p 12700 -d 5
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
@@ -13,10 +16,15 @@ $ g++ -std=c++14 udp-echo-server.cpp -o udp-echo-server
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
-// typedef allows defining own type
+
 #define DEFAULT_SERVER_PORT 8080
+#define MAXSIZE 1000
+
+// typedef allows defining own type
+// delay in secs is used for artificial delay in response
 typedef struct _config {
     int port;
+    int delay_in_secs;
 } Config_t;
 
 // function prototypes mean order of writing functions doesn't matter
@@ -30,14 +38,18 @@ void InterruptionHandler(int sig);
 void GetCmdLineOptions( int argc, char *argv[], Config_t* pconfig) {
     int opt;
     // getopt searches for options with tags, such as "-p" and takes its value such as "nnn"
-    while((opt=getopt(argc, argv, "p:")) != -1) {
+    while((opt=getopt(argc, argv, "p:d:")) != -1) {
         switch(opt) {
             case 'p':
                 // mene osoittimen pconfig osoittamaan rakenteeseen ja käytä sen port-kenttää
                 pconfig->port = atoi(optarg);
                 break;
+            case 'd':
+                pconfig -> delay_in_secs = atoi(optarg);
+                break;
             case '?':
             fprintf(stderr, "Unknown option %c\n", optopt);
+                exit(EXIT_FAILURE);
                 break;
             default:
                 fprintf(stderr, "Usage: udp-echo-server [-p port\n]");
@@ -57,6 +69,7 @@ void InterruptionHandler(int sig) {
     printf("\nDo you really want to quit [y/n]?");
     letter = getchar();
     if(toupper(letter)=='Y') {
+        // todo: close socket
         exit(0);
     } 
     // if user writes something else than "y"
@@ -72,7 +85,10 @@ int main(int argc, char *argv[]) {
     // make a variable called config that is type of Config_t
     Config_t config;
     // save socket id into a variable, when socket is created, OS gives it a number
-    int sockid;
+    int sockid, len, n, msg_cnt;
+    sockid = len = n = msg_cnt = 0;
+    char in_buf[MAXSIZE+4];
+    char out_buf[MAXSIZE+32];
 
     // address of server and client, sockaddr_in contains IP-address and port
     // usually sockaddr_in is used with AF_INET family, aka IPv4 internet addresses
@@ -104,7 +120,49 @@ int main(int argc, char *argv[]) {
     memset(&serveraddr, 0, sizeof(serveraddr));
     memset(&clientaddr, 0, sizeof(clientaddr));
 
-    // TODO code to use socket for something...
+    // Fill in server info
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = INADDR_ANY;
+    serveraddr.sin_port = htons(config.port);
+
+    // make serveraddr_in to serveraddr type before bind
+    if(bind(sockid, (const sockaddr*) &serveraddr, sizeof(serveraddr)) < 0) {
+        perror("Socket bind failed!");
+        exit(EXIT_FAILURE);
+    }
+
+    len = sizeof(clientaddr);
+    // while loop keeps the server running
+    while(true) {
+        // use in_buf array as memory location to receive data
+        if((n = recvfrom(sockid, in_buf, MAXSIZE-1, MSG_WAITALL, (sockaddr*) &clientaddr, (socklen_t*)&len)) < 0) {
+            perror("recvfrom failed");
+            exit(EXIT_FAILURE);
+        }
+        // make sure string ends to the null
+        in_buf[n] = '\0';
+        msg_cnt++;
+        printf("Client req (%d) received from %s using port %u: %s...", 
+            msg_cnt, 
+            inet_ntoa(clientaddr.sin_addr),
+            clientaddr.sin_port,
+            in_buf
+        );
+        // fprintf(file_fp, "", ...)
+        // printf() = fprintf(stdout, "", ...)
+        // use sprintf to print straight into a buffer or file
+        // 9 letters + digit so +20 maxsize for out_buf enough to prevent overflow
+
+        // add additional delay to response
+        if (config.delay_in_secs) {
+            sleep(config.delay_in_secs);
+        }
+        sprintf(out_buf, "Echo [%d]: %s", msg_cnt, in_buf);
+
+        // next step is decide flag where message goes
+        sendto(sockid, out_buf, strlen(out_buf), MSG_CONFIRM, (const sockaddr*)&clientaddr, len);
+        printf("reply sent\n");
+    }
 
     // after done useful stuff with socket, close it
     close(sockid);
